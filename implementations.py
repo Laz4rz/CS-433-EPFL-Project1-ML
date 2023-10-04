c73f059c94e56ebd1a5f1e1a1c270cf34fff8979
Lorenzo Drudi <lori.drudi.11@gmail.com>
2023-10-04 14:36:59 +0200

    GD implementation.

--- implementations.py
+++ implementations.py
@@ -1,5 +1,32 @@
 import numpy as np
+from utils import *
 
+def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
+    """Gradient descent algorithm (GD).
+
+    Args:
+        y ( shape=(N, ) ): _description_
+        tx ( shape=(N,2) ): _description_
+        initial_w ( shape=(2, ) ): initial weight vector
+        max_iters ( int ): number of steps to run
+        gamma ( int ): step size
+    
+    Returns:
+        loss: loss value (scalar) of the last iteration of GD
+        w: model parameters as numpy arrays of shape (2, ) of the last iteration of GD
+    """
+    
+    w = initial_w
+    for _ in range(max_iters):
+        
+        # compute loss, gradient
+        loss = compute_loss(y, tx, w)
+        g = compute_gradient(y, tx, w)
+        
+        # update w by gradient
+        w = w - gamma * g
+
+    return loss, w
 
 def least_squares(y, tx):
     """calculate the least squares."""
