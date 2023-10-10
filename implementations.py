f08dce8695118fe3f1136102ff2e83049ac92821
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2023-10-10 18:36:52 +0200

    add mean_squared_error_sgd implementation

--- implementations.py
+++ implementations.py
@@ -1,6 +1,35 @@
 import numpy as np
 from utils import *
 
+def mean_squared_error_sgd(y, tx, initial_w, batch_size, max_iters, gamma):
+    """The Stochastic Gradient Descent algorithm (SGD).
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic gradient
+        max_iters: a scalar denoting the total number of iterations of SGD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        w: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SGD`   
+        loss: a list of length max_iters containing the loss value (scalar) for each iteration of SGD
+    """
+
+    w = initial_w
+    n_batches = tx.shape[0] // batch_size
+
+    for _ in range(max_iters):
+        grad = 0
+        for batch_y, batch_tx in batch_iter(y, tx, batch_size, n_batches):
+            grad += compute_gradient(batch_y, batch_tx, w)
+        grad = grad / n_batches
+        w = w - gamma * grad 
+        loss = compute_loss(y, tx, w)
+        
+    return w, loss
+
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     """Gradient descent algorithm (GD).
 
