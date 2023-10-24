117390ef4c6551c90ad2f4284db9beff4e704209
laz4rz <boronskimikolaj@gmail.com>
2023-10-24 19:02:40 +0200

    refactor reg_logistic_regression to check ret signature

--- implementations.py
+++ implementations.py
@@ -132,12 +132,9 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
         loss: corresponding loss
     """
     w = initial_w
-    losses = []
     for _ in range(max_iters):
-        # gamma = 1/(n_iter+1) not sure? Gamma is passed as constant in the project description
         for y_b, tx_b in batch_iter(y, tx, batch_size=len(y), num_batches=1):
             gradient = compute_gradient_logistic(y_b, tx_b, w) + 2 * lambda_ * w
             loss = compute_loss_logistic(y_b, tx_b, w) + lambda_ * np.squeeze(w.T.dot(w))
-            losses.append(loss)
             w = w - gamma * gradient
-    return w, losses
+    return w, loss
